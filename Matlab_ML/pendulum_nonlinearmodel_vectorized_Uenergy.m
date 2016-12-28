1d3141b5f442a2129abe19ef600a9e470c565316
Rounak <Rounak@youknowwho.attlocal.net>
2016-12-27 22:47:44 -0500

    stochastic works

---
+++ Matlab_ML/pendulum_nonlinearmodel_vectorized_Uenergy.m
@@ -0,0 +1,17 @@
+function X_new = pendulum_nonlinearmodel_vectorized(X,U_ENERGY,U,F,del_t)
+global n_bar;
+global u_energy_multiplier;
+X_new = zeros(n_bar,2+1);%for u_energy
+m = 0.2;
+b = 0.001;
+I = 0.006;
+g = 9.8;
+l = 0.3;
+
+
+th = X(:,1);
+th_ = X(:,2);
+T = U;
+X_new(:,1) = X(:,1) + del_t* th_;
+X_new(:,2) = X(:,2) + del_t* (-m*g*l*sin(th) -b*th_ + F.*l.*cos(th) + T)/I;
+X_new(:,3) = U_ENERGY - u_energy_multiplier * abs(U);
---
+++ Matlab_ML/report.m
@@ -0,0 +1,8 @@
+%write email to kolmonovsky
+%abstract
+%introduction: lit review: ADP, dynamic programming, neural net based ADP.
+%DCOC: theory
+%approach theory: Value iteration, ADP, gaussian kernel
+%example: Simple pendulum, with ADP: gaussian.
+%tuning?
+%conclusion
